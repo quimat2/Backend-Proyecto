29e51321f59796b24b11b5d91d1550eacdc1ab03
Hector <quimat2@hotmail.com>
2023-08-31 09:40:37 -0600

    commit 31/08/2023

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import cors from 'cors'
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC4cxUpS54adoq7FJycfNeQMZXbOWph714",
+    authDomain: "hagmr-proyecto-backfront.firebaseapp.com",
+    projectId: "hagmr-proyecto-backfront",
+    storageBucket: "hagmr-proyecto-backfront.appspot.com",
+    messagingSenderId: "1007517767506",
+    appId: "1:1007517767506:web:5732a730e0f3aa0dc34c8b"
+};
+
+const firebase = initializeApp(firebaseConfig)
+const db = getFirestore()
+
+// Settings de la aplicacion
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// Creacion de rutas
+app.get('/', async (req, res) => {
+    const Users = await collection(db, 'Users')
+    const listUsers = await getDocs(Users)
+/*     const listUsers = Users.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    })) */
+    res.send({
+        'msg': 'success',
+        'data': listUsers
+    })
+})
+
+// Prendemos el servidor
+app.listen(9000, () => {
+    console.log('Servidor trabajando')
+})
